8ea0b8b89f51bb617c85932fd4ac7ec8ea3879e1
martin-lukacik <martin.lukacik@me.com>
2022-11-27 12:56:35 +0100

    Generify HttpResponse

--- app/src/main/java/com/example/priscillaclient/api/HttpResponse.java
+++ app/src/main/java/com/example/priscillaclient/api/HttpResponse.java
@@ -1,6 +1,5 @@
 package com.example.priscillaclient.api;
 
-public interface HttpResponse {
-    void onUpdate(Object response);
-    //https://stackoverflow.com/questions/12575068/how-to-get-the-result-of-onpostexecute-to-main-activity-because-asynctask-is-a
+public interface HttpResponse<T> {
+    void onUpdate(T response); // TODO generify all calls or ungenerify interface
 }
