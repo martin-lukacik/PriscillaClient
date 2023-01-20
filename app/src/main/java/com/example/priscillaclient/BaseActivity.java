6011669fdabd82bb62eadd3feef8fb390a11dc2b
martin-lukacik <martin.lukacik@me.com>
2023-01-20 22:07:00 +0100

    Logout

--- app/src/main/java/com/example/priscillaclient/BaseActivity.java
+++ app/src/main/java/com/example/priscillaclient/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.example.priscillaclient;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -46,6 +47,17 @@ public class BaseActivity extends AppCompatActivity implements HttpResponse {
                 return true;
             case R.id.all_courses:
                 intent = new Intent(BaseActivity.this, CategoryActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout:
+
+                SharedPreferences settings = getApplicationContext().getSharedPreferences("settings", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("username", null);
+                editor.putString("refresh_token", null);
+                editor.apply();
+
+                intent = new Intent(BaseActivity.this, LoginActivity.class);
                 startActivity(intent);
                 return true;
             default:
