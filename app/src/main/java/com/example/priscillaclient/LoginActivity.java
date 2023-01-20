83d0e2d2e362838ea9388b1954d284dbcd0363c6
martin-lukacik <martin.lukacik@me.com>
2023-01-20 21:39:56 +0100

    Fixed remember user

--- app/src/main/java/com/example/priscillaclient/LoginActivity.java
+++ app/src/main/java/com/example/priscillaclient/LoginActivity.java
@@ -1,27 +1,19 @@
 package com.example.priscillaclient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.priscillaclient.api.HttpResponse;
 import com.example.priscillaclient.api.RequestToken;
 import com.example.priscillaclient.client.Client;
-import com.example.priscillaclient.models.User;
 import com.example.priscillaclient.views.LoadingDialog;
 
 public class LoginActivity extends AppCompatActivity implements HttpResponse {
@@ -47,9 +39,10 @@ public class LoginActivity extends AppCompatActivity implements HttpResponse {
         String username = settings.getString("username", null);
         refresh_token = settings.getString("refresh_token", null);
         if (username != null && refresh_token != null) {
+            CheckBox rememberUser = findViewById(R.id.rememberUser);
+            rememberUser.setChecked(true);
             ((EditText) findViewById(R.id.inputUsername)).setText(username);
-            loadingDialog = new LoadingDialog(LoginActivity.this, "Logging in...");
-            loadingDialog.show();
+            showProgressDialog();
             apiCall = new RequestToken(this);
             apiCall.execute(username, refresh_token, username, "refresh_token");
         }
@@ -61,18 +54,22 @@ public class LoginActivity extends AppCompatActivity implements HttpResponse {
         startActivity(intent);
     }
 
+    public void showProgressDialog() {
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+        );
+
+        loadingDialog = new LoadingDialog(LoginActivity.this, "Logging in...");
+        loadingDialog.show();
+    }
+
     public void performLogin(View view) {
         String username = ((EditText) findViewById(R.id.inputUsername)).getText().toString();
         String password = ((EditText) findViewById(R.id.inputPassword)).getText().toString();
 
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-        );
+        showProgressDialog();
 
-
-        loadingDialog = new LoadingDialog(LoginActivity.this, "Logging in...");
-        loadingDialog.show();
         apiCall = new RequestToken(this);
         apiCall.execute(username, password, username, "password");
     }
@@ -85,20 +82,24 @@ public class LoginActivity extends AppCompatActivity implements HttpResponse {
         loadingDialog.dismiss();
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
-        if (apiCall != null && apiCall.errorMessage != null)
+        if (apiCall != null && apiCall.errorMessage != null) {
             return;
+        }
 
         String username = ((EditText) findViewById(R.id.inputUsername)).getText().toString();
 
         // TODO check if actually logged in (wrong password etc.)
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("settings", 0);
+        SharedPreferences.Editor editor = settings.edit();
         CheckBox rememberUser = findViewById(R.id.rememberUser);
         if (rememberUser.isChecked()) {
-            SharedPreferences settings = getApplicationContext().getSharedPreferences("settings", 0);
-            SharedPreferences.Editor editor = settings.edit();
             editor.putString("username", username);
             editor.putString("refresh_token", Client.getInstance().refresh_token);
-            editor.apply();
+        } else {
+            editor.putString("username", null);
+            editor.putString("refresh_token", null);
         }
+        editor.apply();
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
